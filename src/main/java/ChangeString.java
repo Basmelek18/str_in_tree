1e3a0372a3ddec5d9799fe8d0393f7c8cb6cdca8
Ispaniuk Viacheslav <basmelek18@gmail.com>
2024-04-11 18:50:10 +0300

    only shifts left

--- src/main/java/ChangeString.java
+++ src/main/java/ChangeString.java
@@ -2,12 +2,14 @@ import java.util.*;
 
 public class ChangeString {
     private final String PROBEL = " ";
-    private final String inString;
-    public ChangeString(String inString){
-        this.inString = inString;
-    }
+    private int shiftCount = 0;
+    private boolean flag = false;
+    private List<String> result2 = new ArrayList<>();
+//    public ChangeString(String inString){
+//        this.inString = inString;
+//    }
 
-    public List<Object> toAr(){
+    public static List<Object> toAr(String inString){
         List<String> splitedStrLine = new ArrayList<>(List.of(inString.split("")));
         List<Object> result = new ArrayList<>();
         List<List<Object>> stack = new ArrayList<>();
@@ -42,19 +44,40 @@ public class ChangeString {
         return result;
     }
 
-    public static List<Object> toTree(ArrayList<Object> a) {
-        List<Object> result = new ArrayList<>();
-        int shiftCount = 0;
-        boolean flag = false;
+    public List<?> toTree(List<?> a) {
+        int sumPrevLength = 0;
+        int sumNextLength = 0;
+        int sumL;
+        boolean sumMax = true;
         for (int i = a.size() - 1; i >= 0; i--) {
-            if (a.get(i) instanceof ArrayList<?>) {
-                flag = true;
+            String sum = "";
+            if (a.get(i) instanceof List<?>) {
                 shiftCount++;
+                List<?> b = (List<?>) a.get(i);
+                this.toTree(b);
+                shiftCount--;
+                flag = true;
+                continue;
             }
             if (flag) {
-                ;
+                sum = sum + a.get(i);
+                if (sumMax) {
+                    sumPrevLength = sum.length();
+                    sum = " ".repeat(shiftCount) + sum + "--+";
+                    sumMax = false;
+                }
+                else {
+                    sumNextLength = sum.length();
+                    sumL = sumPrevLength - sumNextLength;
+                    sum = " ".repeat(shiftCount) + sum + "-".repeat(sumL) + "--+";
+                }
+                flag = false;
+                result2.add(sum);
+                continue;
             }
+            sum = " ".repeat(shiftCount) + a.get(i);
+            result2.add(sum);
         }
-        return newTree;
+        return result2;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,13 @@
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String inString = in.nextLine();
-        var pepa = new ChangeString(inString);
-        for (Object underStr : pepa.toAr()) {
-            System.out.println(underStr);
-        }
+        var pepa = new ChangeString();
+        List<Object> pupa = pepa.toAr(inString);
+        System.out.println(pepa.toTree((List<?>) pupa.get(0)));
+
     }
 }
